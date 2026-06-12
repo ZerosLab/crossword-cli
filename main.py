1675ec9eee481c2813aa9bfe25b7a53190fc014d
Willow-Zero <zmckinnis4@gmail.com>
2026-06-12 01:52:37 -0400

    updating

---
+++ main.py
@@ -0,0 +1,35 @@
+# this is crossword solving, crossword generator is iin crossword,py
+
+from testCrossword import puzzle
+
+
+def main():
+    
+
+
+
+
+def placeCrossword(cw,hinum,hidir,solves):
+    for key,data in cw.down
+
+cwPuzzle = {"down":{"test": "an exam"},
+	"across"{"stay":"to remain"},
+	"intersect":{"test":["stay",2,0]}}
+
+
+def printCrossword(crossword,hinum,hidirecction,solves):
+     # create an array to hold the crossword
+    # place wrds in array
+    # trim empty rows and collumns
+    # print array with no surrounding stuff
+    X = "X"
+    puzzleArray = [
+        ["X","X","X","X"],
+        ["X","X","X","X"],
+        ["X","X","X","X"],
+        ["X","X","X","X"]]
+    for row in puzzleArray:
+        for elem in row:
+            print(elem,end="")
+        print("")
+printCrossword([],-1,-1,-1)
---
+++ testCrossword.py
@@ -0,0 +1,5 @@
+puzzle=	{"down":[{"fires":"ancient means of cooking. often destroy buildings"},
+		{"atlanta":"city in georgia. an ancient greek heroinen misspelled."}],
+	"across":[{"electricity":"one of the fundamental forces. foundational to modern technology"}],
+	"intersect":{"fires":["electricity",3,0],"atlanta":["electricity",1,4]}
+	}
