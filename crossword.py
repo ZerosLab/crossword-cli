55f45f5c82779e66090ec26ae94a49d3dc409d70
WillowZero <zmckinnis4@gmail.com>
2026-06-29 18:09:56 -0400

    idk ngl just syncing things up

--- crossword.py
+++ crossword.py
@@ -1,38 +1,20 @@
 import defslist as d
 
-print("Select desired crossword puzzle difficulty (enter number between 1 and 10)")
-
-while not (diff) :
-    diff = (input("> "))
-    try:
-        diff = int(diff)
-    except:
-        
-
-
-emptySquare="[_]"
-nullSquare="[ ]"
-
-numWordsVert = int(diff*2)
-numWordsHoriz = diff*2
-
-i=0
-while i<numWordsVert:
-    i+=1
+def generateCrossword():
+    print("Select desired crossword puzzle difficulty (enter number between 1 and 10)")
+    
+    while not (diff) :
+        diff = (input("> "))
+        try:
+            diff = int(diff)
+        except:
+            diff = False
+    numWordsVert = int(diff*2)
+    numWordsHoriz = diff*2
+    
+    i=0
+    while i<numWordsVert:
+        i+=1
     # todo randomly select vverticla words with no repeats
-
-# todo loop and randomly select horiz words w no repeats
-
-# todo find shared letters and determine placement 
-
-# todo arrange letters in array
-
-# todo print array as a crossword 
-
-# todo print clues and numbers 
-
-# todo allow interaction - selecting clue number and entring answer
-
-# todo show filled answers on screen 
-
-# todo verify correctness of answers
+    # todo loop and randomly select horiz words w no repeats
+    # todo find shared letters and determine placement
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # this is crossword solving, crossword generator is iin crossword,py
 
 from testCrossword import puzzle
-
+#from crossword import generateCrossword
 
 def main():
     cwPuzzle = {"down":[["test", "an exam",False]],
 	        "across":[["stay","to remain",False]],
 	"intersect":{"test":["stay",2,0]}}
+    #cwPuzzle = generateCrossword()
     done = False
     while not done:
         printCrossword(cwPuzzle,-1,-1,-1)
@@ -57,8 +58,8 @@ def checkvalid(cw): # checks validity of guesses given overlaps in the puzzle.
                 if palabra[2] and word[2]:
                     try:
                         if palabra[2][cw["intersect"][word[2]][2]] != word[2][cw["intersect"][word[2]][1]]:
-                            print(palabra[2][cw["intersect"][word[2]][2]])
-                            print(word[2][cw["intersect"][word[2]][1]])
+                            #print(palabra[2][cw["intersect"][word[2]][2]])
+                            #print(word[2][cw["intersect"][word[2]][1]])
                             return False
                     except:
                         print("error")
@@ -88,13 +89,37 @@ def printCrossword(crossword,hinum,hidirecction,solves):
     # trim empty rows and collumns
     # print array with no surrounding stuff
     X = "X"
-    puzzleArray = [["X"]*4]*4
+    placements = {}
+    maxDwn = 1
+    for word in crossword["down"]:
+        maxDwn += len(word)
+    maxAcc = 1
+    for word in crossword["across"]:
+        maxAcc+=len(word)
+    puzzleArray = [["X"]*maxAcc]*maxDwn
+    #place first down at 0,0
+    placements[crossword["down"][0][0]]=(0,0,0)
+        
+    # figure first intersection, place and shift if necessary
+    # loop 
     for row in puzzleArray:
         for elem in row:
             print(elem,end="")
         print("")
     print(crossword)
 
+def moveR(cw):
+    for word in cw.keys():
+        cw[word][0]+=1
 
+def moveL(cw):
+    for word in cw.keys():
+        cw[word][0]-=1
+def moveD(cw):
+    for word in cw.keys():
+        cw[word][1]+=1
+def moveU(cw):
+    for word in cw.keys():
+        cw[word][1]-=1
 
 main()
